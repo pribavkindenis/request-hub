f92e532847a437b724822c344b274901d248e403
Denis Pibavkin <pribavkindenis@gmail.com>
2020-03-01 13:27:10 +0400

    Docker dev environment initialized

---
+++ request-hub-backend/src/main/java/com/pribavkindenis/requesthub/RequestHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pribavkindenis.requesthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestHubBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestHubBackendApplication.class, args);
+    }
+
+}
---
+++ request-hub-backend/src/test/java/com/pribavkindenis/requesthub/RequestHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pribavkindenis.requesthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
